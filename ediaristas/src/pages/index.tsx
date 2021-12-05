3373fbf3fbff55ba5a8066c68ee5d97811773dc4
Vitor Honna <vitorhonna@gmail.com>
2021-12-05 12:02:30 -0300

    Create styled components and config custom theme

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from 'next'
+import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment'
+import PageTitle from 'ui/components/data-display/PageTitle/PageTitle'
 
 const Home: NextPage = () => {
   return (
     <div>
+      <SafeEnvironment />
+      <PageTitle
+        title={'Conheça os profissionais'}
+        subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
+      />
 
     </div>
   )
