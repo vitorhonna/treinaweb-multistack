ae66e3f8658da27fc240f88897fe4a59a7b70504
Vitor Honna <vitorhonna@gmail.com>
2021-12-07 10:08:57 -0300

    Create app web (back)

---
+++ backend/ediaristas_workshop/web/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .forms import diarista_form
+
+# Create your views here.
+
+def cadastrar_diarista(request):
+    form_diarista = diarista_form.DiaristaForm()
+    return render(request, 'form_diarista.html', {'form_diarista': form_diarista})
