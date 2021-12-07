ff1a6c9db4224589c34e69520d52c43b64b54ba1
Vitor Honna <vitorhonna@gmail.com>
2021-12-07 11:27:24 -0300

    Add workers list, template engine and styling with bootstrap

--- backend/ediaristas_workshop/web/urls.py
+++ backend/ediaristas_workshop/web/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import cadastrar_diarista
+from .views import cadastrar_diarista, listar_diaristas
 
 urlpatterns = [
-    path('cadastrar_diarista', cadastrar_diarista, name='cadastrar_diarista')
+    path('cadastrar_diarista', cadastrar_diarista, name='cadastrar_diarista'),
+    path('listar_diaristas', listar_diaristas, name='listar_diaristas'),
 ]
--- backend/ediaristas_workshop/web/views.py
+++ backend/ediaristas_workshop/web/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import diarista_form
+from .models import Diarista
 
 # Create your views here.
 
@@ -8,6 +9,12 @@ def cadastrar_diarista(request):
         form_diarista = diarista_form.DiaristaForm(request.POST, request.FILES)
         if form_diarista.is_valid():
             form_diarista.save()
+            return redirect('listar_diaristas')
     else:
         form_diarista = diarista_form.DiaristaForm()
     return render(request, 'form_diarista.html', {'form_diarista': form_diarista})
+
+
+def listar_diaristas(request):
+    diaristas = Diarista.objects.all()
+    return render(request, 'lista_diaristas.html', {'diaristas': diaristas})
